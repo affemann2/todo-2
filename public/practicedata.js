2cabc044592d7f93a30ed226d413fe9f4144b244
Shaniqua <zahirbasrai@yahoo.com>
2017-03-17 17:49:49 -0700

    added database

---
+++ public/practicedata.js
@@ -0,0 +1,13 @@
+{
+  "users": {
+    "user": {
+      "task": "boob",
+      "completed": "false",
+    },
+    "ghopper": { ... },
+    "eclarke": { ... }
+  }
+}
+
+// Get a reference to the database service
+var database = firebase.database();
